2b9ab621ce1286c0b0a33e311fb8c8bdbc729f9e
zafarbek <zafarbek.alibekov@bk.ru>
2022-06-12 20:58:12 +0500

    bismillah

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -3,36 +3,36 @@ import React, {useState} from "react";
 import { Rating } from 'react-simple-star-rating'
 
 const CardGroup = () => {
-  const [rating1, setRating1] = useState(50)
+  // const [rating1, setRating1] = useState(50)
   
-  const tooltipArray = [
-    'Terrible',
-    'Terrible+',
-    'Bad',
-    'Bad+',
-    'Average',
-    'Average+',
-    'Great',
-    'Great+',
-    'Awesome',
-    'Awesome+'
-  ]
-  const fillColorArray = [
-    '#f17a45',
-    '#f17a45',
-    '#f19745',
-    '#f19745',
-    '#f1a545',
-    '#f1a545',
-    '#f1b345',
-    '#f1b345',
-    '#f1d045',
-    '#f1d045'
-  ]
+  // const tooltipArray = [
+  //   'Terrible',
+  //   'Terrible+',
+  //   'Bad',
+  //   'Bad+',
+  //   'Average',
+  //   'Average+',
+  //   'Great',
+  //   'Great+',
+  //   'Awesome',
+  //   'Awesome+'
+  // ]
+  // const fillColorArray = [
+  //   '#f17a45',
+  //   '#f17a45',
+  //   '#f19745',
+  //   '#f19745',
+  //   '#f1a545',
+  //   '#f1a545',
+  //   '#f1b345',
+  //   '#f1b345',
+  //   '#f1d045',
+  //   '#f1d045'
+  // ]
   
  
   
-  const handleRating1 = (rate) => setRating1(rate)
+  // const handleRating1 = (rate) => setRating1(rate)
   return (
     <div>
       {" "}
@@ -77,7 +77,7 @@ const CardGroup = () => {
                   Ermäßigung mit Karte
                 </span>
               </div>
-              <div className=" flex-wrap  columns-5 p-4 flex items-center text-sm text-gray-600">
+              {/* <div className=" flex-wrap  columns-5 p-4 flex items-center text-sm text-gray-600">
 
               <Rating
                     onClick={handleRating1}
@@ -90,8 +90,8 @@ const CardGroup = () => {
                     fillColorArray={fillColorArray}
                     className='fill-current'
                   />
-              </div>
-              {/* <div className="p-4 flex items-center text-sm text-gray-600">
+              </div> */}
+              <div className="p-4 flex items-center text-sm text-gray-600">
                 <svg
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
@@ -128,7 +128,7 @@ const CardGroup = () => {
                   <path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path>
                 </svg>
                 <span className="ml-2">34 Bewertungen</span>
-              </div> */}
+              </div>
             </a>
           </div>
         </div>
